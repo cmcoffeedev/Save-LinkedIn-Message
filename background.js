0cb97506572d49cfc0e58a822698a3ee43e0120d
Christopher Coffee <chrisc1891@gmail.com>
2021-05-24 19:42:04 -0500

    dont execute sheets api in service worker. It was sending every request twice

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
-
-chrome.runtime.onInstalled.addListener(() => {
-
-
-    chrome.tabs.onActivated.addListener(async info => {
-        const tab = await chrome.tabs.get(info.tabId);
-
-        let isLinkedIn = false;
-        if (tab.url.startsWith('https://linkedin.com/messaging/thread/' || 'https://www.linkedin.com/messaging/thread/')) {
-            isLinkedIn = true;
-        }
-        isLinkedIn
-            ? chrome.action.enable(tab.tabId)
-            : chrome.action.disable(tab.tabId);
-    });
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//
+//
+//     chrome.tabs.onActivated.addListener(async info => {
+//         const tab = await chrome.tabs.get(info.tabId);
+//
+//         console.log(`tabUrl is ${tab.url}`)
+//         let isLinkedIn = false;
+//         if (tab.url.includes('https://linkedin.com/messaging/thread/' || 'https://www.linkedin.com/messaging/thread/')) {
+//             isLinkedIn = true;
+//         }
+//         if (isLinkedIn) {
+//             chrome.action.enable(tab.tabId)
+//         } else {
+//             chrome.action.disable(tab.tabId)
+//         }
+//
+//     });
+// });
